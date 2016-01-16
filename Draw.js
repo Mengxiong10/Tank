2ecd32dc4841a9304beb4060d563b8fc5db3c7e4
Mengxiong10 <15623530290@163.com>
2016-01-16 19:38:00 +0800

    Revert "First Stage"
    This reverts commit 5e100c432af03c686c343f99aad453be9769656c.

--- Draw.js
+++ Draw.js
@@ -5,18 +5,18 @@ function drawTank(tank) {
 	c.rotate(tank.direct*Math.PI/2);
 	// 坦克的长宽是20
 	c.fillStyle = tank.color[0];
-	c.fillRect(-tank.w/2, -tank.h/2, tank.w/4, tank.h);
-	c.fillRect(tank.w/4, -tank.h/2, tank.w/4, tank.h);
-	c.fillRect(-tank.w/4+1, -tank.h/3, tank.w/2-2, 2/3*tank.h);
+	c.fillRect(-10, -10, 5, 20);
+	c.fillRect(5, -10, 5, 20);
+	c.fillRect(-4, -6, 8, 12);
 	c.fillStyle = tank.color[1];
 	c.beginPath();
-	c.arc(0, 0, tank.w/4-3, 0, 2 * Math.PI);
+	c.arc(0, 0, 3, 0, 2 * Math.PI);
 	c.fill();
 	c.beginPath();
-	c.lineWidth = tank.w/8;
+	c.lineWidth = 2;
 	c.strokeStyle = tank.color[2];
 	c.moveTo(0, 0);
-	c.lineTo(0, -tank.h/2);
+	c.lineTo(0, -10);
 	c.closePath();
 	c.stroke();
 	c.restore();
@@ -24,24 +24,7 @@ function drawTank(tank) {
 // 画子弹
 function drawBullet(bullet){
 	c.beginPath();
-	c.arc(bullet.x,bullet.y,4,0,2*Math.PI);//子弹半径是4
+	c.arc(bullet.x,bullet.y,4,0,2*Math.PI);//子弹半径是2
 	c.fillStyle = bullet.color;
 	c.fill();
 }
-
-//画地图
-
-function drawMap(num) {
-	for (var i = 0; i < map[num].length; i++) {
-		for(var j = 0; j<map[num][i].length; j++){
-			if (map[num][i][j] == 1) {
-				c.fillStyle = '#BCBCBC';
-				c.fillRect(30*j,30*i,30,30);
-				c.fillStyle = '#FFFFFF';
-				c.fillRect(30*j+6,30*i+6,18,18);
-			}
-		}
-	}
-}
-
-
